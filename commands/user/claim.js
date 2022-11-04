a4b31764eb9bd325e880588379bc815f8aa7b16b
NuxL <91557468+Dilucal@users.noreply.github.com>
2022-11-04 20:16:31 +0700

    Update claim.js

--- commands/user/claim.js
+++ commands/user/claim.js
@@ -11,7 +11,7 @@ exports.run = {
          client.sendMessageModify(m.chat, Func.texted('bold', `Selamat, kamu mendapat Uang 1JT dan 50 limit 🔮️`), m, {
             largeThumb: true,
             thumbnail: await Func.fetchBuffer('https://telegra.ph/file/e4954937673ef75e467b2.jpg'),
-            url: 'https://chat.whatsapp.com/EARvthLENgw2yxhDUKDuMr'
+            url: 'https://chat.whatsapp.com/CmJkktfVp0H2Gl4v11VOLW'
          })
          global.db.users.point += 1000000
          user.limit += 50
@@ -20,7 +20,7 @@ exports.run = {
          client.sendMessageModify(m.chat, Func.texted('bold', `Kamu sudah melakukan claim silahkan klaim kembali di jam berikutnya*\n\n*⏱️ : ${Func.toTime(timeout)}️`), m, {
             largeThumb: true,
             thumbnail: await Func.fetchBuffer('https://telegra.ph/file/2d7a56edbda6128633a06.jpg'),
-            url: 'https://chat.whatsapp.com/EARvthLENgw2yxhDUKDuMr'
+            url: 'https://chat.whatsapp.com/CmJkktfVp0H2Gl4v11VOLW'
          })
       }
    },
