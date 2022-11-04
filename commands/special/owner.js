d745cf3b53f53bed52ebd980621fee82a1fa3a38
NuxL <91557468+Dilucal@users.noreply.github.com>
2022-11-04 15:09:40 +0700

    Update owner.js

--- commands/special/owner.js
+++ commands/special/owner.js
@@ -7,11 +7,11 @@ exports.run = {
       // if (command == 'owner') return client.reply(m.chat, `Interkasi owner dengan user via WhatsApp sudah tidak tersedia lagi, jika ingin upgrade premium atau ada hal penting lainnya silahkan kirim pesan melalui email dengan subjek *"MINT"*\n\n✉️ : contact@neoxr.my.id`, m)
       if (command == 'owner') {
          const contact = [{
-            name: 'Anti psatir',
-            number: '6285807264974'
+            name: 'Armandd_',
+            number: '6287823406145'
          }]
          const msg = await client.sendContact(m.chat, contact, m)
-         await client.reply(m.chat, `Chat aja gausah sungkan" bang`, msg)
+         await client.reply(m.chat, `Info penting owner bot sangat ganteng`, msg)
       }
       // if (command == 'owners') return client.sendContactArr(m.chat, global.db.setting.owners, m)
    },
