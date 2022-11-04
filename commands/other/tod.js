2b4b57add10e94edac6b6948c185cd9c26b8d50f
NuxL <91557468+Dilucal@users.noreply.github.com>
2022-11-04 20:25:00 +0700

    Update tod.js

--- commands/other/tod.js
+++ commands/other/tod.js
@@ -9,7 +9,7 @@ exports.run = {
    		    client.sendMessageModify(m.chat, `*❏ T R U T H*\n\n${txt}\n\n𝘁𝗿𝘂𝘁𝗵 𝗼𝗿 𝗱𝗮𝗿𝗲`, m, {
             largeThumb: true,
             thumbnail: await Func.fetchBuffer('https://telegra.ph/file/ba55209f87abd428edbf7.jpg'),
-            url: 'https://chat.whatsapp.com/EARvthLENgw2yxhDUKDuMr'
+            url: 'https://chat.whatsapp.com/CmJkktfVp0H2Gl4v11VOLW'
          })
 		} else if (command == 'dare') {
 			let senja = JSON.parse(fs.readFileSync('./media/json/dare.json'))
@@ -18,7 +18,7 @@ exports.run = {
 			client.sendMessageModify(m.chat, `*❏ D A R E*\n\n${txt}\n\n𝘁𝗿𝘂𝘁𝗵 𝗼𝗿 𝗱𝗮𝗿𝗲`, m, {
             largeThumb: true,
             thumbnail: await Func.fetchBuffer('https://telegra.ph/file/ba55209f87abd428edbf7.jpg'),
-            url: 'https://chat.whatsapp.com/EARvthLENgw2yxhDUKDuMr'
+            url: 'https://chat.whatsapp.com/CmJkktfVp0H2Gl4v11VOLW'
          })
 		}
 	},
